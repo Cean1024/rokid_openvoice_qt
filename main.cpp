06b71dec3ebed740b45ee2a678e784c31484f223
kean <kean.wu@hansong-china.com>
2018-01-11 14:07:51 +0800

    change main to add callback function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <unistd.h>
 #include "speechsdk.h"
-
+#include "ttssdk.h"
 
 using namespace std;
+
+
+void Handle_speech_result(speech::SpeechResult &Result);
+void Handle_tts_result(speech::TtsResult &Result);
 
 int main()
 {
@@ -14,19 +18,20 @@ int main()
     popts.port = 443;
     popts.branch = "/api";
     // 认证信息，需要申请
-    popts.key = "770AE250B1F8488E8EBD0E4E3EE01424";
-    popts.device_type_id = "BE947B8C753043A4B2543605A5668B88";
-    popts.secret = "E58FDBB7A4EF41209E09C022758F55BF";
+    popts.key = "17E0ECB86CC446BFB62FF0A6A3641B0B";
+    popts.device_type_id = "89A9AD627E124316BD8392D749B5C73B";
+    popts.secret = "1ECCFBCB2298416693A5790304BDF9C9";
     // 设备名称，类似昵称，可自由选择，不影响认证结果
     popts.device_id = "keantestaudio";
 
-    SpeechSdk tts_sdk,speech_sdk;
-    tts_sdk.init(popts,sdktype_tts);
-    speech_sdk.init(popts,sdktype_speech);
+    SpeechSdk speech_sdk;
+    TtsSdk tts_sdk;
+    filehandler filehdler;
+    speech_sdk.init(popts , Handle_speech_result);
+    tts_sdk.init(popts , Handle_tts_result);
     std::string input;
     while(1)
     {
-
         speech_sdk.speek(input);
         std::cout << "please input words:";
         std::cin >> input;
@@ -35,3 +40,74 @@ int main()
     return 0;
 }
 
+void Handle_speech_result(speech::SpeechResult &Result)
+{
+    /*
+    SPEECH_RES_INTER = 0,
+    SPEECH_RES_START,
+    SPEECH_RES_ASR_FINISH,
+    SPEECH_RES_END,
+    SPEECH_RES_CANCELLED,
+    SPEECH_RES_ERROR
+*/
+    switch (Result.type) {
+    case speech::SPEECH_RES_ERROR:{
+        printf("Handler ------speech------> err:%d\n",Result.err);
+       };break;
+    case speech::SPEECH_RES_INTER: {
+        printf("Handler ------speech------> get voice\n");
+        std::cout<<"[SPEECH_RES_INTER]"<<std::endl;
+        std::cout<<"nlp:"<<Result.nlp<<std::endl;
+        std::cout<<"asr:"<<Result.asr<<std::endl;
+        std::cout<<"action:"<<Result.action<<std::endl;
+        std::cout<<"extra:"<<Result.extra<<std::endl;
+
+       };break;
+    case speech::SPEECH_RES_START:{
+        printf("Handler ------speech------> voice start\n");
+
+            std::cout<<"[SPEECH_RES_START]"<<std::endl;
+            std::cout<<"nlp:"<<Result.nlp<<std::endl;
+            std::cout<<"asr:"<<Result.asr<<std::endl;
+            std::cout<<"action:"<<Result.action<<std::endl;
+            std::cout<<"extra:"<<Result.extra<<std::endl;
+
+       };break;
+    case speech::SPEECH_RES_CANCELLED:
+    case speech::SPEECH_RES_END:{
+        printf("Handler ------speech------> voice end\n");
+        std::cout<<"[SPEECH_RES_END]"<<std::endl;
+        std::cout<<"nlp:"<<Result.nlp<<std::endl;
+        std::cout<<"asr:"<<Result.asr<<std::endl;
+        std::cout<<"action:"<<Result.action<<std::endl;
+        std::cout<<"extra:"<<Result.extra<<std::endl;
+
+       };break;
+
+    case speech::SPEECH_RES_ASR_FINISH: {
+        std::cout<<"action:"<<Result.action<<std::endl;
+    };break;
+    }
+}
+
+
+void Handle_tts_result(speech::TtsResult &Result)
+{
+    switch (Result.type) {
+    case speech::TTS_RES_ERROR:{
+        printf("Handler ------tts------> err:%d\n",Result.err);
+       };break;
+    case speech::TTS_RES_VOICE:{
+        printf("Handler ------tts------> get voice\n");
+       };break;
+    case speech::TTS_RES_START:{
+        printf("Handler ------tts------> voice start\n");
+
+       };break;
+    case speech::TTS_RES_CANCELLED:
+    case speech::TTS_RES_END:{
+        printf("Handler ------tts------> voice end\n");
+        std::cout<<Result.voice;
+       };break;
+    }
+}
