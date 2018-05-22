00991d54e2af66a95b9899b21874612b76b7ff92
kean <kean.wu@hansong-china.com>
2018-05-22 16:38:55 +0800

    upgrade netserver class

--- netserver.cpp
+++ netserver.cpp
@@ -2,7 +2,16 @@
 
 netserver::netserver()
 {
+    welcomeSocket =-1;
+    newSocket =-1;
+    initserver(LOCALIP,PORT);
+}
 
+netserver::netserver(std::__cxx11::string ip, int port)
+{
+    welcomeSocket =-1;
+    newSocket =-1;
+    initserver(ip,port);
 }
 
 netserver::~netserver()
@@ -10,3 +19,80 @@ netserver::~netserver()
 
 }
 
+r_status netserver::initserver(std::string ip,int port) {
+
+  int ret;
+
+
+  welcomeSocket = socket(AF_INET, SOCK_STREAM, 0);
+  if(welcomeSocket<0){perror("socket");return FAILED;}
+
+
+  /* Address family = Internet */
+  serverAddr.sin_family = AF_INET;
+
+  serverAddr.sin_port = htons(port);
+
+  serverAddr.sin_addr.s_addr = inet_addr(ip.c_str());
+
+  memset(serverAddr.sin_zero, '0', sizeof serverAddr.sin_zero);
+
+  /*---- Bind the address struct to the socket ----*/
+  ret = bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+  if(ret<0){perror("bind");return FAILED;}\
+
+
+  return SUCCESS;
+}
+
+r_status netserver::listenandaccept()
+{
+    if(welcomeSocket<0){return FAILED;}
+    /*---- Listen on the socket, with 5 max connection requests queued ----*/
+    if(listen(welcomeSocket,5) ==0 ) printf("Listening\n");
+    else{ printf("Error\n"); return FAILED;}
+
+    /*---- Accept call creates a new socket for the incoming connection ----*/
+    addr_size = sizeof serverStorage;
+    newSocket = accept(welcomeSocket, (struct sockaddr *) &serverStorage, &addr_size);
+    if(newSocket <0)return FAILED;
+    LOGOUT("client comming");
+
+    return SUCCESS;
+}
+
+r_status netserver::listenandaccept(sockaddr_storage &client)
+{
+    if(welcomeSocket<0) {return FAILED;}
+    /*---- Listen on the socket, with 5 max connection requests queued ----*/
+    if(listen(welcomeSocket,5) ==0 ) printf("Listening\n");
+    else{ printf("Error\n"); return FAILED;}
+
+    /*---- Accept call creates a new socket for the incoming connection ----*/
+    socklen_t addr_size = sizeof client;
+    if(newSocket>0)close(newSocket);
+    newSocket = accept(welcomeSocket, (struct sockaddr *) &client, &addr_size);
+    if(newSocket <0)return FAILED;
+    LOGOUT("client comming");
+
+    return SUCCESS;
+}
+
+void netserver::closeclient()
+{
+    if(newSocket>0) close(newSocket);
+    newSocket = -1;
+}
+
+r_status netserver::getdata(char *buf,int len)
+{
+    if(newSocket<0){return FAILED;}
+    return recv(newSocket,buf,len,0);
+}
+
+r_status netserver::senddata(char *buf,int len)
+{
+    if(newSocket<0){return FAILED;}
+    /*---- Send message to the socket of the incoming connection ----*/
+    return send(newSocket,buf,len,0);
+}
--- netserver.h
+++ netserver.h
@@ -1,12 +1,36 @@
 #ifndef NETSERVER_H
 #define NETSERVER_H
+#include "common.h"
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
 
+#define PORT 9009
+#define LOCALIP "127.0.0.1"
 
 class netserver
 {
 public:
     netserver();
+    netserver(std::string ip,int port);
     ~netserver();
+
+    r_status listenandaccept();
+    r_status listenandaccept(sockaddr_storage &client);
+    r_status getdata(char *buf,int len);
+    r_status senddata(char *buf,int len);
+    void closeclient();
+
+protected:
+    r_status initserver(std::string ip,int port);
+
+private:
+    int welcomeSocket, newSocket;
+    struct sockaddr_in serverAddr;
+    struct sockaddr_storage serverStorage;
+    socklen_t addr_size;
 };
 
 #endif // NETSERVER_H
