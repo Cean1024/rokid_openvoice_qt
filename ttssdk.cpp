f52404ddbb740f15560ce04d3afeaffe275424aa
kean <kean.wu@hansong-china.com>
2018-05-23 15:29:40 +0800

    add tts execute status to reinit it when failed with speek interface

--- ttssdk.cpp
+++ ttssdk.cpp
@@ -3,6 +3,7 @@
 TtsSdk::TtsSdk()
 {
     handleresult=NULL;
+    voicehandle = vh_finish;
 }
 
 TtsSdk::~TtsSdk()
@@ -11,29 +12,57 @@ TtsSdk::~TtsSdk()
 }
 int TtsSdk::speek(std::string strings)
 {
-     return tts->speak(strings.c_str());
+     int ret;
+     vh_status vhs;
+     int times =  RETRY_TIME;
+speek_start:
+
+     set_vh_status(vh_start);
+     ret = tts->speak(strings.c_str());
+     while( true ) {
+         vhs = get_vh_status();
+         if( vhs == vh_finish) break;
+         else if( vhs == vh_err  ) {
+             reinit();
+
+             if( times > 0)
+                 times--;
+             else break;
+
+             goto speek_start;
+         }
+         usleep(100000);
+     }
+     return ret;
+
 }
 
-int TtsSdk::init(speech::PrepareOptions &popts, callback_tts_func func, void *data)
+void TtsSdk::reinit()
 {
+    tts->prepare(popts);
+}
 
+int TtsSdk::init( speech::PrepareOptions &popts, callback_tts_func func, void *data )
+{
     handleresult = func;
     tts->prepare(popts);
+    this->popts = popts;
     this->data = data;
 
     // 在prepare后任意时刻，都可以调用config修改配置
     // 默认配置codec = PCM, declaimer = ZH
     // 下面的代码将codec修改为OPU2，declaimer保持原状不变
-/*
+
     std::shared_ptr<speech::TtsOptions> topts = speech::TtsOptions::new_instance();
-    topts->set_codec(speech::Codec::OPU);
+    topts->set_codec(speech::Codec::PCM);
     tts->config(topts);
-*/
+
     // 使用tts
     //int32_t id = tts->speak("我是会说话的机器人,我最爱吃的食物是机油,最喜欢的运动是聊天");
     //int32_t id = tts->speak("机器人");
 
     // 获取tts结果。api阻塞式，应考虑在独立线程中运行。
+
     ptr.start(*this);
 }
 
@@ -45,6 +74,16 @@ void TtsSdk::run()
         if (!tts->poll(result))
             break;
         // 处理result
-        handleresult(result,data);
+        handleresult(result,data,this);
+        switch(result.type) {
+        case speech::TTS_RES_ERROR: {
+            set_vh_status(vh_err);
+        };break;
+        case speech::TTS_RES_CANCELLED:
+        case speech::TTS_RES_END:{
+            set_vh_status(vh_finish);
+        };break;
+
+        }
     }
 }
--- ttssdk.h
+++ ttssdk.h
@@ -6,7 +6,13 @@
 #include "Poco/Thread.h"
 #include "Poco/Runnable.h"
 #include "common.h"
-typedef void (*callback_tts_func)(rokid::speech::TtsResult &Result,void *data);
+typedef void (*callback_tts_func)(rokid::speech::TtsResult &Result,void *data,void *data2);
+#define RETRY_TIME 5
+enum vh_status{
+    vh_start,
+    vh_finish,
+    vh_err
+};
 
 using namespace rokid;
 class TtsSdk: public Poco::Runnable
@@ -16,13 +22,24 @@ public:
     ~TtsSdk();
     int init(speech::PrepareOptions &popts ,callback_tts_func func,void *data);
     int speek(std::__cxx11::string strings);
-
+    void set_vh_status(vh_status vhs)
+    {
+        voicehandle=vhs;
+    }
+    vh_status get_vh_status()
+    {
+        return voicehandle;
+    }
+protected:
     virtual void run();
+    void reinit();
 private:
     std::shared_ptr<speech::Tts> tts = speech::Tts::new_instance();
+    speech::PrepareOptions popts;
     callback_tts_func handleresult;
     Poco::Thread ptr;
     void * data;
+    vh_status voicehandle;
 
 };
 
